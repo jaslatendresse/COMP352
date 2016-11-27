fb794d477e4eae7081769aa3dff24137ba9b443c
Jasmine Latendresse <latendresse.jasmine@gmail.com>
2016-11-27 18:08:21 -0500

    whole assignment pushed

--- 352FINAL/src/Sequence.java
+++ 352FINAL/src/Sequence.java
@@ -1,16 +1,25 @@
-
 public class Sequence implements DataStructure {
 
+/* ============================================================ Instance Variables ============================================================ */
 	public Entry first; 
 	public Entry last;
-	public int size; 
-	public int[] keysToSort;
-	public int[] temp;
+	public int size;
+	public int[] keysToSort; // used for the merge sort
+	public int[] temp; // used for the merge sort
 	
+/* ================================================================== Methods ================================================================= */	
+	/**
+	 * Verifies if the sequence is empty.
+	 * @return true if the sequence contains at least one entry,
+	 * 		   false if it contains no entry.
+	 */
 	public boolean isEmpty(){
 		return size == 0; 
 	}
 	
+	/**
+	 * Default constructor.
+	 */
 	public Sequence(){
 		this.first = null;
 		this.last = null; 
@@ -19,21 +28,20 @@ public class Sequence implements DataStructure {
 	
 	@Override
 	public void put(String key, int value) {
-		Entry e = new Entry(); 
+		Entry e = new Entry();
 		
 		if(isEmpty()){
 			first = e; 
 			last = e;
 		}
 		else{
-			e.next = null; 
+			e.next = null;
 			e.prev = last; 
 			last.next = e;
 			last = e; 
 		}
 		e.key = key;
 		e.value = value; 
-		e.index = size; 
 		size++;
 	}
 	
@@ -43,65 +51,73 @@ public class Sequence implements DataStructure {
 		Entry entry = new Entry(); 
 		Entry temp = null; 
 		
-		for(int i = 0; i < size; i++){
+		for(int i = 0; i < size; i++){ // loops through the sequence 
 			if(entry.key.equals(key)){
 				temp = entry; 
-				entry.prev.next = temp.next; 
-				entry.next.prev = temp.prev; 
-				
-				for(int j = entry.index; j < size; j++){
-					temp.index--; 
-					temp = temp.next;
-				}
-				size--; 
-				return; 
+				entry.prev.next = temp.next;
+				entry.next.prev = temp.prev; 		
 			}
 			else{
 				entry = entry.next;
 			}
 		}
-		
+		size--; 
 		System.out.println("Entry with key " + key + " was not found.");
 	}
 
 	@Override
 	public int get(String key) {
-		Entry entry = new Entry(); 
+		Entry entry = this.first; 
 		for(int i = 0; i < size; i++){
 			if(entry.key.equals(key)){
 				return entry.value; 
 			}
 			entry = entry.next;
 		}
+		System.out.println("Key was not found");
 		return -1;
 	}
 	
 	@Override
+	public String getFirstKey() {
+		if (this.first == null)
+			return null;
+		else
+			return this.first.getKey();
+	}
+	
+	@Override
 	public String nextKey(String key){
-		Entry entry = new Entry();
-		for(int i = 0; i < size; i++){
-			if(entry.getKey().equals(key) && entry.getNext().getKey() != null){
-				return entry.getNext().getKey();
-			}
+		Entry entry = this.first;
+		
+		while(entry.getNext() != null) {		
+			if (entry.getKey().equals(key))
+				return entry.getNext().getKey();		
+			entry = entry.getNext();
 		}
+		System.out.println("Next key was not found");
 		return null;
 	}
 	
 	@Override
 	public String prevKey(String key){
-		Entry entry = new Entry();
-		for(int i = 0; i < size; i++){
-			if(entry.getKey() == key && entry.getPrev().getKey() != null){
-				return entry.getPrev().getKey();
-			}
+		Entry entry = this.last;	
+		
+		while(entry.getPrev() != null) {		
+			if (entry.getKey().equals(key))
+				return entry.getPrev().getKey();		
+			entry = entry.getPrev();
 		}
 		return null; 
 	}
+		
 	
 	@Override
 	public int[] sort(){
 		Entry entry = first;
 		keysToSort = new int[size];
+	
+	// filling keysToSort with the unordered keys
 		for(int i = 0; i<size; i++){
 			if(entry.getKey() != null){
 				keysToSort[i] = Integer.parseInt(entry.getKey());
@@ -109,19 +125,32 @@ public class Sequence implements DataStructure {
 			}
 		}
 		this.temp = new int[size];
+		
+	// ordering the keys
 		mergesort(0, size - 1);
 		return keysToSort; 
 	}
-	
+
+	/**
+	 * Merge sorts a sequence according to the keys of its entries.
+	 * @param low the lowest key in the range
+	 * @param high the highest key in the range
+	 */
 	private void mergesort(int low, int high){
 		if(low < high){
-			int mid = low + (high - low)/2;
-			mergesort(low, mid);
-			mergesort(mid + 1, high);
-			merge(low, mid, high);
+			int mid = low + (high - low)/2; // average of the low and high
+			mergesort(low, mid); 			// merge sort the lower-half
+			mergesort(mid + 1, high); 		// merge sort the upper-half
+			merge(low, mid, high); 			// merge the combined two halves
 		}
 	}
 	
+	/**
+	 * Merges the merge sorted halves of a sequence
+	 * @param low lowest key in the sequence
+	 * @param mid mid value key in the sequence
+	 * @param high highest key in the sequence
+	 */
 	private void merge(int low, int mid, int high){
 		for(int i = low; i <=high; i++){
 			temp[i] = keysToSort[i];
@@ -145,12 +174,16 @@ public class Sequence implements DataStructure {
 }
 
 class Entry{
-	protected String key;
+/* ============================================================ Instance Variables ============================================================ */
+	protected String key; // used as an identifier
 	protected Integer value;
-	protected Integer index; 
 	protected Entry next;
 	protected Entry prev;
 	
+/* ================================================================== Methods ================================================================= */
+	/**
+	 * Default constructor
+	 */
 	public Entry(){
 		this.key = null;
 		this.value = null;
--- 352FINAL/src/SmartULS.java
+++ 352FINAL/src/SmartULS.java
@@ -1,33 +1,132 @@
-import java.util.Arrays;
-import java.util.Random;
-
+import java.util.*;
+import java.io.File;
+import java.io.IOException;
 public class SmartULS {
-	private final static int THRESHOLD = 1000;
-	boolean isHashTable = false;
+	private final static int THRESHOLD = 1000; 
+	boolean isBST = false;
 	boolean isSequence = false;
 	DataStructure DS;
 	
-	public static void main(String[] args){
+	public static void main(String[] args) throws IOException{
 	
-		/*
-		 * If using prevKey or nextKey, we have to make sure to add
-		 * if(nextKey < 0) System.out.println("next key could not be found);
-		 * 
-		 * 
-		 * What is left to do: 
-		 * nextKey (BinarySearchTree)
-		 * prevKey (BinarySearchTree)
-		 * rangeKeys (SmartULS)
-		 * Test
-		 * 
-		 */
+		String stringSize;
+		Scanner userInput = new Scanner(System.in);
+		boolean inputIsInt = false;
+		
+	// Testing with test files
+		try{
+			Scanner in = new Scanner(new File("src/file1.txt"));
+			SmartULS uls = new SmartULS(999);
+			System.out.println("SmartULS created");
+			
+			int randomVal = 0; 
+			
+			System.out.println("Inserting entries to the SmartULS");
+			String newKey;
+			while(in.hasNextInt()){
+				try{
+					newKey = String.format("%08d", in.nextInt());
+					uls.add(newKey, randomVal);
+					randomVal++;
+				}
+				catch(InputMismatchException e){
+					System.out.println("Key format is wrong");
+				}
+			}
+			System.out.println("Entries added to SmartULS");
+			System.out.println();	
+			in.close();
+		}catch(IOException e){
+			System.out.println("File not found");
+		}
+		
+		try{
+			Scanner in = new Scanner(new File("src/file2.txt"));
+			SmartULS uls = new SmartULS(999);
+			System.out.println("SmartULS created");
+			
+			int randomVal = 0; 
+			
+			System.out.println("Inserting entries to the SmartULS");
+			String newKey;
+			while(in.hasNextInt()){
+				try{
+					newKey = String.format("%08d", in.nextInt());
+					uls.add(newKey, randomVal);
+					randomVal++;
+				}
+				catch(InputMismatchException e){
+					System.out.println("Key format is wrong");
+				}
+			}
+			System.out.println("Entries added to SmartULS");
+			System.out.println();
+			in.close();
+		}catch(IOException e){
+			System.out.println("File not found");
+		}
+		
+		try{
+			Scanner in = new Scanner(new File("src/file3.txt"));
+			SmartULS uls = new SmartULS(999);
+			System.out.println("SmartULS created");
+			
+			int randomVal = 0; 
+			
+			System.out.println("Inserting entries to the SmartULS");
+			String newKey;
+			while(in.hasNextInt()){
+				try{
+					newKey = String.format("%08d", in.nextInt());
+					uls.add(newKey, randomVal);
+					randomVal++;
+				}
+				catch(InputMismatchException e){
+					System.out.println("Key format is wrong");
+				}
+			}
+			System.out.println("Entries added to SmartULS");
+			System.out.println();
+			in.close();
+		}catch(IOException e){
+			System.out.println("File not found");
+		}
+		
+	//Testing with user input
+		do{
+			System.out.println("Enter the deisred size of the SmartULS");
+			stringSize = userInput.next();
+			if(isNum(stringSize)){
+				inputIsInt = true; 
+			}
+		}while(inputIsInt == false);
 	}
 	
+	/**
+	 * Verifies if a string contains only numbers.
+	 * @param str string to be verified
+	 * @return true if all the characters are numbers,
+	 * 		   false otherwise
+	 */
+	public static boolean isNum(String str){
+		try{
+			int i = Integer.parseInt(str);
+		}
+		catch(NumberFormatException e){
+			return false;
+		}
+		return true;
+	}
+	
+	/**
+	 * Constructor for SmartULS
+	 * @param size number of expected entries of the SmartULS
+	 */
 	public SmartULS(int size){
 		setSmartThresholdULS(size);
-		if(isHashTable){
-			DS = new HashTable();
-			System.out.println("The data structure used is Hashtable");
+		if(isBST){
+			DS = new BinarySearchTree();
+			System.out.println("The data structure used is Binary Search Tree");
 		}
 		else if(isSequence){
 			DS = new Sequence();
@@ -35,36 +134,73 @@ public class SmartULS {
 		}
 	}
 	
+	/**
+	 * Sets the type of data structure that will be used by the SmartULS
+	 * @param size number of expected entries of the SmartULS
+	 */
 	public void setSmartThresholdULS(int size){
 		if(size >= THRESHOLD){
-			isHashTable = true;
+			isBST = true;
 		}
 		else{
 			isSequence = true; 
 		}
 	}
 	
+	/**
+	 * Adds an entry to the SmartULS
+	 * @param key Key of the new entry
+	 * @param value Value of the new entry
+	 */
 	public void add(String key, int value){
 		DS.put(key, value);
 	}
 	
+	/**
+	 * Removes an entry in the SmartULS object.
+	 * @param key key of the entry to be removed
+	 */
 	public void remove(String key){
 		DS.remove(key);
 	}
 	
+	/**
+	 * Finds the value of a given entry in the SmartULS object.
+	 * @param key key of the given entry
+	 * @return value of the given entry
+	 */
 	public int getValues(String key){
 		return DS.get(key);
 	}
 	
+	/**
+	 * Finds the key succeeding a given entry in the SmartULS object.
+	 * @param key key of the given entry
+	 * @return key of the entry succeeding the given entry
+	 */
 	public String nextKey(String key){
+		if(isBST){
+			DS.sort();
+		}
 		return DS.nextKey(key);
 	}
 	
+	/**
+	 * Finds the key preceding a given entry in the SmartULS object.
+	 * @param key  key of the given entry
+	 * @return key of the entry preceding the given entry
+	 */
 	public String prevKey(String key){
-		
+		if(isBST){
+			DS.sort();
+		}
 		return DS.prevKey(key);
 	}
 	
+	/**
+	 * Generates a random key for a given entry in the SmartULS object.
+	 * @return random key
+	 */
 	public int generate(){
 		Random r = new Random();
 		int randomKey;
@@ -72,15 +208,39 @@ public class SmartULS {
 		do{
 			randomKey = 1000000 + r.nextInt(90000000);
 			strVal = Integer.toString(randomKey);
+			
 		}while(DS.get(strVal) != -1);
 		
 		return randomKey;
 	}
 	
+	/**
+	 * Prints the sorted keys if the entries in the SmartULS object.
+	 */
 	public void allKeys(){
 		int[] sortedKeys = DS.sort();
-		
 		System.out.println(Arrays.toString(sortedKeys));
 	}
+	
+	/**
+	 * Finds the number of keys in the SmartULS object that are within a given range.
+	 * @param key1 lower bound of the range
+	 * @param key2 upper bound of the range
+	 * @return number of keys inside the range
+	 */
+	public int rangeKey(String key1, String key2) {
+		int numbOfKeys = 0;
+		String key = this.DS.getFirstKey();
+		
+		while (key != null) {
+			if( key.compareTo(key1) > 0 && key.compareTo(key2) < 0)
+				numbOfKeys++;
+			key = this.DS.nextKey(key);
+		}
+		
+		return numbOfKeys;
+	}
+	
+	
 	
 }
